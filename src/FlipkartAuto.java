29185948a6931d683c26a7affee68bf537954d68
ARK <ARK@192.168.2.12>
2019-05-01 21:41:38 +0530

    Fixes

---
+++ src/FlipkartAuto.java
@@ -0,0 +1,64 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class FlipkartAuto {
+
+	WebDriver driver = new ChromeDriver();
+	@Before
+	public void setUp() throws Exception {
+		Log.startTestCase("Flipkart Automation Testing");
+		Log.info("Driver Setup initiated");
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		try {
+			
+			driver.quit();
+			Log.info("WebDriver Wipe Completed");
+			Log.endTestCase("Facebook Account Signin");
+		} catch (Exception e) {
+		}
+	}
+
+	@Test
+	public void signinCheck() throws Exception {
+		try {
+			Log.info("Test Started");
+			driver.findElement(By.id("email")).sendKeys("9659135936");
+			driver.findElement(By.id("pass")).sendKeys("arkMiracles%12");
+			driver.findElement(By.id("u_0_2")).click();
+			Thread.sleep(10000);
+			
+			if (fnIsElementAvailable(driver, By.xpath("//a[@href='https://www.facebook.com/settings?ref=mb&drop']")))
+			{	Log.info("Login Success");
+				driver.findElement(By.xpath("//a[@href='https://www.facebook.com/settings?ref=mb&drop']")).click();
+				driver.findElement(By.xpath("//span[contains(text(),'Log Out')]")).click();
+				Thread.sleep(5000);
+				Log.info("Logout");
+			}
+			else
+				Log.error("Login Failed... Check Username Password");
+		} catch (Exception e) {
+		}
+	}
+
+	private boolean fnIsElementAvailable(WebDriver driver2, By by) {
+		try {
+			driver.findElement(by);
+			return true;
+		} catch (NoSuchElementException NE) {
+			return false;
+		}
+	}
+
+}
